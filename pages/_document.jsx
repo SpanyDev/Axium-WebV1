70da446e5a3e602f0e789be6ec61b78c6087fbd5
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-07-28 22:30:09 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,12 +13,9 @@ class MyDocument extends Document {
             name="description"
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta property="og:url" content="https://axium.me" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Axium" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +23,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Axium is built to protect and monitor your Discord servers. Completely innovative systems are used."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="Axium" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Axium"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
