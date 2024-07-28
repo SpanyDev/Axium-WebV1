e9ea368d0ece9980d4b01c7b2edfa70e53524007
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-07-28 22:39:55 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -1,23 +1,24 @@
-export default function Privacy() {
+export default function Terms() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
-  <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
-        <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
-        <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
-        <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
-        <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
-        <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
-    
-        </div>
+            <h1 className="font-semibold text-3xl text-white/75">Terms of Service for Axium</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 28.07.2024 🆕️</p>
+            <div className="animateHeader text-white text-opacity-40 mt-5">
+                <p>⊳ To use the Axium bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+                <div className="py-3"></div>
+                <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Axium</a> bot when using certain commands from the bot commands.</p>
+                <div className="py-3"></div>
+                <p>⊳ Every user can use the Axium bot however they want, as long as they do not intend to harm.</p>
+                <div className="py-3"></div>
+                <p>⊳ You cannot use certain commands at the same time while using Axium.</p>   
+                <div className="py-3"></div>
+                <p>⊳ Axium bot does not save any data based on users.</p>
+                <div className="py-3"></div>
+                <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+                <div className="py-3"></div>
+                <p>⊳ By using Axium, you agree to comply with the <a className="text-white" href="https://discord.com/developers/docs/policies-and-agreements/developer-terms-of-service">Discord Developer Terms of Service</a>.</p>
             </div>
+        </div>
     );
 };
