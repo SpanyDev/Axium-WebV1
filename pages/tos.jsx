438bdd71fe44a8eb415e49849af1526996cbc2c4
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-07-28 22:41:15 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,21 +1,24 @@
 export default function Tos() {
-  return (
-    <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
-  <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
-<div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
-<div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
-<div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
-<div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
-<div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
-</div>
-    </div>
-  );
-}
+
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl text-white/75">Terms of Service for Axium</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 28.07.2024 🆕️</p>
+            <div className="animateHeader text-white text-opacity-40 mt-5">
+                <p>⊳ To use the Axium bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+                <div className="py-3"></div>
+                <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Axium</a> bot when using certain commands from the bot commands.</p>
+                <div className="py-3"></div>
+                <p>⊳ Every user can use the Axium bot however they want, as long as they do not intend to harm.</p>
+                <div className="py-3"></div>
+                <p>⊳ You cannot use certain commands at the same time while using Axium.</p>   
+                <div className="py-3"></div>
+                <p>⊳ Axium bot does not save any data based on users.</p>
+                <div className="py-3"></div>
+                <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+                <div className="py-3"></div>
+                <p>⊳ By using Axium, you agree to comply with the <a className="text-white" href="https://discord.com/developers/docs/policies-and-agreements/developer-terms-of-service">Discord Developer Terms of Service</a>.</p>
+            </div>
+        </div>
+    );
+};
