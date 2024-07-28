e195b553cb082e1f5699bc8f8a05d52fdf9c75f2
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-07-28 22:28:11 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,29 +37,22 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ZcDprCrFTn",
+      href: "https://discord.gg/fS72UGCEXD",
     },
     {
       link: true,
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/oauth2/authorize?client_id=965432989360193566&permissions=8&scope=applications.commands%20bot",
     },
        {
       link: true,
       name: "Vote",
       icon: "fa fa-plus",
       activeIcon: "fa fa-plus",
-      href: "https://top.gg/bot/774043716797071371/vote",
+      href: "https://top.gg/bot/965432989360193566/vote",
     },
-    {
-      link: true,
-      name: "Partners",
-      icon: "fal fa-handshake",
-      activeIcon: "fa fa-handshake",
-      href: "/partners",
-    }
   ]
 
   return (
