536effd174c9307c2a0c386d0f6b905ef1bc4deb
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-07-28 22:51:36 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -81,20 +81,6 @@ Vote Axium</a>
           </div>
       </div>
 
-
-
-      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
-              <>
-                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://media.discordapp.net/attachments/1230924580563189831/1267204069441929438/CercevesizLogo2.png?ex=66a7ef41&is=66a69dc1&hm=fdd6db70b6e99c4edd706fbdea88ab65865cea72aa9258e8062491e80d45711f&=&format=webp&quality=lossless&width=258&height=258"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Axium enhances your server with top-tier moderation and protection features, making it the best choice for managing your Discord community</p>
-                </div>
-                <img src="https://media.discordapp.net/attachments/1230924580563189831/1267204069441929438/CercevesizLogo2.png?ex=66a7ef41&is=66a69dc1&hm=fdd6db70b6e99c4edd706fbdea88ab65865cea72aa9258e8062491e80d45711f&=&format=webp&quality=lossless&width=258&height=258" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
-              </>
-      
-        </div>
-
       </>
   )
 }
