021641ad24ceab2b28206a6513dc685941294235
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-07-28 22:54:20 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,14 +30,7 @@ export default function Index() {
   <Link href={"https://top.gg/bot/965432989360193566/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote Axium</a>
-  </Link>
-        </div>
-
-        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
-        <Image width="600" height="600" src="/img/gift-left.png"  />
-        </div>
-        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
-        <Image  width="600" height="600" src="/img/gift-right.png"  />
+  </Link>	
         </div>
       </div>
 
